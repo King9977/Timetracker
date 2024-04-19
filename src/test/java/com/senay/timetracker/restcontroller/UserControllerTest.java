91da1d4423e0925b322f7a791de477bab46d10d7
King9977 <kaya.senay@bbzbl-it.ch>
2024-04-19 11:14:03 +0200

    TestController

--- src/test/java/com/senay/timetracker/restcontroller/UserControllerTest.java
+++ src/test/java/com/senay/timetracker/restcontroller/UserControllerTest.java
@@ -14,7 +14,6 @@ import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.web.client.RestTemplate;
